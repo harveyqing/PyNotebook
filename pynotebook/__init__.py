3dd1210e0a65113fa20ad7325d877268d5fb1a19
harveyqing <harveyqing@gmail.com>
2014-03-07 16:39:56 +0800

    <add some common files>

---
+++ pynotebook/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+	pynotebook
+	~~~~~~~~~~
+
+	pynotebook application package
+""" 
---
+++ pynotebook/settings.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+	pynotebook.settings
+	~~~~~~~~~~~~~~~~~~~
+
+	pynotebook settings module
+"""
+
+DEBUG = True
+
+SECRET_KEY = 'pynotebook-secret-key'
+CSRF_ENABLED = True
+
+SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost:3306/pynotebook'
+CELERY_BROKER_URL = 'redis://localhost/0'
+
+OPENID_PROVIDERS = [
+	{ 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' }
+]
