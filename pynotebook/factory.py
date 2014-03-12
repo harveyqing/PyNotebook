7fb48d7212f9df26e3357f298ac543d78394b07d
harveyqing <harveyqing@gmail.com>
2014-03-12 22:48:22 +0800

    create some helper modules

---
+++ pynotebook/factory.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    pynotebook.factory
+    ~~~~~~~~~~~~~~~~~~
+    
+    pynotebook factory module 
+"""
+
+import os
+
+from celery import Celery
+from flask import Flask
+ 
